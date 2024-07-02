74fcdc368277c71a1cea8e9bbf6c2e70941de6b1
adieb <mrizqiamiruladieb@gmail.com>
2024-07-02 17:54:15 +0700

    feat: add validation options

--- get_list_users.py
+++ get_list_users.py
@@ -3,6 +3,9 @@ import requests
 import json
 from jsonschema import validate
 
+# Set the base url
+BASE_URL = "https://reqres.in"
+
 def test_get_list_of_users():
 
     jsonSchema = {
@@ -74,16 +77,24 @@ def test_get_list_of_users():
   ]
 }
 
-    url = "https://reqres.in/api/users?page=2"
-
     # Making a get request 
-    response = requests.get(url)
+    response = requests.get(BASE_URL + "/api/users?page=2")
 
     # Convert json into dictionary 
     respJson = response.json()
 
+    # Convert json into string 
+    string_resp_body = str(respJson)
+
+    print("\n------------Request------------")
+
+    print("Method : ", response.request)
+    print("Url : ", response.url)
+
+    print("\n------------Response------------")
+
     # print response status code
-    print("\nResponse Status Codes: ", response.status_code)
+    print("Response Status Codes: ", response.status_code, response.reason)
 
     # print response 
     # print("\nResponse: ", response)
@@ -91,9 +102,22 @@ def test_get_list_of_users():
     # Pretty Printing JSON string back 
     print("Response Body:\n", json.dumps(respJson, indent=4, sort_keys=True))
 
+    # get response headers
+    content_type = response.headers.get("Content-Type")
+
     # assert
-    assert response.status_code == 200
+    assert response.status_code == 200, "Unexpected status code: " + str(response.status_code)
     assert respJson.get('page') == 2
+
+    # Asserting Response Headers
+    assert content_type == "application/json; charset=utf-8", "Unexpected Content-Type: " + content_type 
+
+    # is empty response body
+    assert len(string_resp_body) != 0, "Unexpected Response Body Length " + str(len(string_resp_body))
+    assert len(str(respJson.get("page"))) != 0, "Unexpected Response Body Length " + str(len(str(respJson.get("page"))))
+
+    # check the substring in the response body 
+    assert bool("2" in string_resp_body) == True
 
     validate(instance=respJson, schema=jsonSchema)
     # No error, the JSON is valid.
