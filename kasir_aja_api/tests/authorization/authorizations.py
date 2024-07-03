4f6cb17903774f6849b6e0f7518d89565492ae88
adieb <mrizqiamiruladieb@gmail.com>
2024-07-03 19:13:23 +0700

    feat: end to end test - authorizations

--- kasir_aja_api/tests/authorization/authorizations.py
+++ kasir_aja_api/tests/authorization/authorizations.py
@@ -187,6 +187,8 @@ def test_success_login():
     # declare the global variables
     global email_request
     global password_request
+    global access_token_response
+    global refresh_token_response
 
     payload = {
         "email": email_request,
@@ -221,6 +223,8 @@ def test_success_login():
 
     # Parse the response body
     email_response = respJson.get('data').get('user').get('email')
+    access_token_response = respJson.get('data').get('accessToken')
+    refresh_token_response = respJson.get('data').get('refreshToken')
 
     # get response headers
     content_type = response.headers.get("Content-Type")
@@ -241,6 +245,168 @@ def test_success_login():
 
     # check the substring in the response body
     assert bool("Authentication berhasil ditambahkan" in string_resp_body) == True
+
+    validate(instance=respJson, schema=jsonSchema)
+    # No error, the JSON is valid.
+
+def test_success_refresh_token():
+
+    jsonSchema = {
+        "$schema": "http://json-schema.org/draft-07/schema#",
+        "title": "Generated schema for Root",
+        "type": "object",
+        "properties": {
+            "status": {
+                "type": "string"
+            },
+            "message": {
+                "type": "string"
+            },
+            "data": {
+                "type": "object",
+                "properties": {
+                    "accessToken": {
+                        "type": "string"
+                    }
+                },
+                "required": [
+                    "accessToken"
+                ]
+            }
+        },
+        "required": [
+            "status",
+            "message",
+            "data"
+        ]
+    }
+
+    # declare the global variables
+    global access_token_response
+    global refresh_token_response
+
+    payload = {
+        "refreshToken": refresh_token_response
+    }
+
+    access_token = access_token_response
+
+    headers = {"Authorization": f"Bearer {access_token}",
+               "Content-Type": "application/json"}
+
+    # Making a put request
+    response = requests.put(BASE_URL + "/authentications", json=payload, headers=headers)
+
+    # Convert json into dictionary
+    respJson = response.json()
+
+    # Convert json into string
+    string_resp_body = str(respJson)
+
+    print("\n------------Request------------")
+
+    print("Method : ", response.request)
+    print("Url : ", response.url)
+    print("Request Body :\n", json.dumps(payload, indent=4, sort_keys=False))
+
+    print("\n------------Response------------")
+
+    # print response status code
+    print("Response Status Codes: ", response.status_code, response.reason)
+
+    # Pretty Printing JSON string back
+    print("Response Body:\n", json.dumps(respJson, indent=4, sort_keys=False))
+
+    # get response headers
+    content_type = response.headers.get("Content-Type")
+
+    # assert
+    assert response.status_code == 200, "Unexpected status code: " + \
+        str(response.status_code)
+
+    # Asserting Response Headers
+    assert content_type == "application/json; charset=utf-8", "Unexpected Content-Type: " + content_type
+
+    # is empty response body
+    assert len(string_resp_body) != 0, "Unexpected Response Body Length " + \
+        str(len(string_resp_body))
+    assert len(str(respJson.get("status"))) != 0, "Unexpected Response Body Length " + \
+        str(len(str(respJson.get("status"))))
+
+    # check the substring in the response body
+    assert bool("Access Token berhasil diperbarui" in string_resp_body) == True
+
+    validate(instance=respJson, schema=jsonSchema)
+    # No error, the JSON is valid.
+
+def test_success_logout():
+
+    jsonSchema = {
+        "$schema": "http://json-schema.org/draft-07/schema#",
+        "title": "Generated schema for Root",
+        "type": "object",
+        "properties": {
+            "status": {
+                "type": "string"
+            },
+            "message": {
+                "type": "string"
+            }
+        },
+        "required": [
+            "status",
+            "message"
+        ]
+    }
+
+    # declare the global variables
+    global refresh_token_response
+
+    payload = {
+        "refreshToken": refresh_token_response
+    }
+
+    # Making a delete request
+    response = requests.delete(BASE_URL + "/authentications", json=payload)
+
+    # Convert json into dictionary
+    respJson = response.json()
+
+    # Convert json into string
+    string_resp_body = str(respJson)
+
+    print("\n------------Request------------")
+
+    print("Method : ", response.request)
+    print("Url : ", response.url)
+    print("Request Body :\n", json.dumps(payload, indent=4, sort_keys=False))
+
+    print("\n------------Response------------")
+
+    # print response status code
+    print("Response Status Codes: ", response.status_code, response.reason)
+
+    # Pretty Printing JSON string back
+    print("Response Body:\n", json.dumps(respJson, indent=4, sort_keys=False))
+
+    # get response headers
+    content_type = response.headers.get("Content-Type")
+
+    # assert
+    assert response.status_code == 200, "Unexpected status code: " + \
+        str(response.status_code)
+
+    # Asserting Response Headers
+    assert content_type == "application/json; charset=utf-8", "Unexpected Content-Type: " + content_type
+
+    # is empty response body
+    assert len(string_resp_body) != 0, "Unexpected Response Body Length " + \
+        str(len(string_resp_body))
+    assert len(str(respJson.get("status"))) != 0, "Unexpected Response Body Length " + \
+        str(len(str(respJson.get("status"))))
+
+    # check the substring in the response body
+    assert bool("Refresh token berhasil dihapus" in string_resp_body) == True
 
     validate(instance=respJson, schema=jsonSchema)
     # No error, the JSON is valid.
