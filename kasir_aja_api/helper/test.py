ce6fe80f3e9d8031d76109785c15f2b75a26c657
adieb <mrizqiamiruladieb@gmail.com>
2024-07-04 10:09:03 +0700

    feat: create helper for get user id

--- kasir_aja_api/helper/test.py
+++ kasir_aja_api/helper/test.py
@@ -1,3 +1,5 @@
 from get_access_token import success_get_access_token
+from get_user_id import success_get_user_id
 
-print(success_get_access_token())
+# print(success_get_access_token())
+print(success_get_user_id())
